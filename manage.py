7ae0b41a8799e3f1e5c29fb5a8d22ca0b79242b7
harsh kumar <harsh.kumar@crofarm.in>
2023-09-13 20:52:09 +0530

    fix

--- manage.py
+++ manage.py
@@ -3,15 +3,15 @@
 import os
 import sys
 
-
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'newApiProject.settings')
     try:
         from django.core.management import execute_from_command_line
+        execute_from_command_line(sys.argv)
     except ImportError as exc:
        "error"
-    execute_from_command_line(sys.argv)
+   
 
 
 if __name__ == '__main__':
